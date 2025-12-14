70e07bc70720488179b46dcfe2ef8985b0c74125
vaibhavtaywade <taywadevaibhav63@gmail.com>
2025-12-14 10:49:25 +0530

    Added Backend Deployed URL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import type { Sweet, AuthResponse } from "../types"
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+const API_URL = import.meta.env.VITE_API_URL || "https://sweet-shop-3-kf10.onrender.com"
 
 const api = axios.create({
   baseURL: API_URL,
